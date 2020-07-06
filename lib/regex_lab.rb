99fe032caab18d787356955e2123b91ffb4220a4
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-07-06 22:18:49 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -16,7 +16,12 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if 
+    #starting with a capital letter and ending with first_word_capitalized_and_ends_with_punctuation
+    true
+  else
+    false
+  end
 end
 
 def valid_phone_number?(phone)
