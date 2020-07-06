11830da2f05d25f0fff3ee07f407515dce737797
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-07-06 22:39:53 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -25,7 +25,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if text.scan((?:\+?|\b)[0-9]{10}\b)
+  if text.scan()
     true
   else
     false
