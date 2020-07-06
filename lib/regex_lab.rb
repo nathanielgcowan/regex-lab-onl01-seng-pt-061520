4549f7949b746d390050b9ca4d9dc1a525cf0124
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-07-06 22:27:04 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -19,6 +19,8 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
     first_letter = text.scan(/\w+/)[0]
   if (text.scan(/\./)) && (first_letter == first_letter.upcase)
     true
+  else
+    false
   end
 end
 
