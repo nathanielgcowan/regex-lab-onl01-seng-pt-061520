e625ff3320100569f719a75c0202d9382b07c40f
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-07-06 22:25:08 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -16,12 +16,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
- if (text.scan(/\./)) && (first_letter == first_letter.upcase)
-    true
-  else  
-    false
-  end
+  
 end
 
 def valid_phone_number?(phone)
