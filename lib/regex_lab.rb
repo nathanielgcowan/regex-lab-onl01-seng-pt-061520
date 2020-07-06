ad18d428c9a1cc7f8dcb577f2ae9ffa27ba8f828
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-07-06 22:26:18 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -16,7 +16,10 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  
+    first_letter = text.scan(/\w+/)[0]
+  if (text.scan(/\./)) && (first_letter == first_letter.upcase)
+    true
+  end
 end
 
 def valid_phone_number?(phone)
